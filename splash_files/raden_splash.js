02a70a989c0d31d3458a01c8852d3eb96e3d1d16
Chris Bunk <chrisbunk@gmail.com>
2015-09-04 12:34:14 -0400

    Dennis Verison

---
+++ splash_files/raden_splash.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	
+	function validateEmail (email) {
+		var email_regex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
+		return email_regex.test(email);
+	}
+	
+	$('#subscribe').click(function(event) {
+		event.preventDefault();
+		console.log("subscribe clicked");
+		
+		var emailAddress = $('#email').val();
+		console.log(emailAddress);
+		
+		if (validateEmail(emailAddress)) {
+			alert("Email is valid");
+		}
+		else {
+			alert("Please enter a valid email address");
+			$('#error').removeClass('hidden');
+		}
+	});
+	
+});
