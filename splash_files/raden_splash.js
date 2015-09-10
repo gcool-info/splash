0e6fa6aeb075eb254d6b41f2a13126e4ce40de18
Chris Bunk <chrisbunk@gmail.com>
2015-09-10 13:37:36 -0400

    support for multiple color schemes compelted

--- splash_files/raden_splash.js
+++ splash_files/raden_splash.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 	
+  var bgcolors = ["#000000","#e45824","#afd9e8"];                
+  var rand = Math.floor(Math.random()*bgcolors.length);           
+  $('body').css("background-color", bgcolors[rand]);
+  if(rand==1){
+      $('div.portrait').html('<img src="./splash_files/raden_bg_768_orangeBG.png" class="img-responsive center-block">');
+      $('div.landscape').html('<img src="./splash_files/shorty-bag-orangeBG.png" class="img-responsive center-block">');
+  }
+  if(rand==2){
+      $('body').css("color", '#494949');
+      $('.radenlogo').attr('src','./splash_files/bluebg-grey-logo.png');
+      $('.subButton').css("color",'#FFF');
+      $('div.portrait').html('<img src="./splash_files/raden_bg_768_blueBG.png" class="img-responsive center-block">');
+      $('div.landscape').html('<img src="./splash_files/shorty-bag-blueBG.png" class="img-responsive center-block">');
+  }
+	
 	function validateEmail (email) {
 		var email_regex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
 		return email_regex.test(email);
